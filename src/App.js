2623040efbbd2aa638742b07442647512a2bd820
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-03-02 18:18:40 +0900

    리액트] React로 푸딩비 블로그 만들기
    글 영역(article)을 누르면 모달창이 나오는 기능을 추가하였다! 추가적으로 모달창 안에 간단한 글이 나오도록 object를 만들어보았고, props를 통해 object의 value값을 넣어서 모달창 안에서 출력하도록 만들었다! 가장 밑에 나오는 출력물은 nav의 모달창도 한 번 만들어보려고 하는 것이다!
    
    ★ 구현한 기능 
    
    - 글 목록의 글 클릭 시 열고 닫을 수 있는 모달창 구현
      · 글 주제와 비슷한 색상으로 모달창 배경 색 반영
    - 모달창의 내용 추가
      · 각각의 글마다 다른 모달창의 내용이 나오도록 object 형성 및 value값 반영
    
    ★ 학습한 내용
    
    - 리액트에서 object를 만드는 방법
    - 리액트에서 object의 value값을 가져오는 방법(props를 이용하여 응용했다네~!!!v😊v)
    
    ★ 다음 목표
    - 하트 버튼 클릭 시 모달창 나오는 기능 개선
    - nav 카테고리 모달창 기능 만들기
    - + 클릭 시 글을 발행해주는 글 목록 추가 기능 만들기

--- src/App.js
+++ src/App.js
@@ -3,95 +3,150 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComment } from "@fortawesome/free-solid-svg-icons";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  let [title] = useState(["우유 푸딩 만들기", "2023년 푸딩비가 먹은 딸기 음료 모음展", "파르페 만들기 도전기"]);
+  let [title] = useState([
+    "우유 푸딩 만들기",
+    "2023년 푸딩비가 먹은 딸기 음료 모음展",
+    "파르페 만들기 도전기",
+  ]);
   let [time, setTime] = useState("0000.00.00.00:00");
-  let [like, setLike] = useState([0, 0, 0])
+  let [like, setLike] = useState([0, 0, 0]);
+  const modalLetter = {
+    0: "우유 푸딩 레시피 재료 - 흑설탕 125g / 우유 400ml / 계란 2개",
+    1: "pudding__github--icon.png",
+    2: "삐쀼루뺘빰",
+  };
+  let [bgColor, setBgColor] = useState([
+    "lightgoldenrodyellow",
+    "lightpink",
+    "lightblue",
+    "lightcyan",
+    "lightsalmon",
+  ]);
+  let [modal, setModal] = useState(false);
+  let [object, setObject] = useState(0);
 
-    const clock = () => {
-      let today = new Date();
-      let year = today.getFullYear();
-      let month = String(today.getMonth()+1).padStart(2, "0");
-      let day = String(today.getDate()).padStart(2, "0");
-      let hour = String(today.getHours()).padStart(2, "0");
-      let min = String(today.getMinutes()).padStart(2, "0");
+  const clock = () => {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = String(today.getMonth() + 1).padStart(2, "0");
+    let day = String(today.getDate()).padStart(2, "0");
+    let hour = String(today.getHours()).padStart(2, "0");
+    let min = String(today.getMinutes()).padStart(2, "0");
 
-      let nowTime = `${year}.${month}.${day}.${hour}:${min}`
-      setTime(nowTime)
-  }
-   
+    let nowTime = `${year}.${month}.${day}.${hour}:${min}`;
+    setTime(nowTime);
+  };
+
   const currentClock = () => {
-      setInterval(clock, 1000)
-    }
-    
-  currentClock()
+    setInterval(clock, 1000);
+  };
+
+  currentClock();
 
   const likeCount = (i) => {
     let likeCopy = [...like];
     likeCopy[i] = likeCopy[i] + 1;
-    setLike(likeCopy)
-  }
+    setLike(likeCopy);
+  };
 
   return (
     <div className="App">
-
       <header className="blog__header">
-
-        <div className='profile-box'>
-        <img className='profile-icon' src='./pudding__github--icon.png' />
-        <div className='name'>
-          <span>푸딩비</span>
-          <span>코코딩 뿌딩딩</span>
-        </div>
-        </div>
-    
-        <div className='letter-number'>
-          <h3>전체 글 ({ title.length })</h3>
+        <div className="profile-box">
+          <img className="profile-icon" src="./pudding__github--icon.png" />
+          <div className="name">
+            <span>푸딩비</span>
+            <span>코코딩 뿌딩딩</span>
           </div>
+        </div>
 
-            <nav className="hamburger__wrapper">
-               <button className="nav__hamburger">
-                   <span></span>
-                   <span></span>
-                  <span></span>
-              </button>
-            </nav>
+        <div className="letter-number">
+          <h3>전체 글 ({title.length})</h3>
+        </div>
+
+        <nav className="hamburger__wrapper">
+          <button className="nav__hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </nav>
       </header>
 
-      {
-          title.map(function (a, i) {
-            return (
-               <article>
-                <h3 className="title"> {title[i]} </h3>
-                <div className="subtitle">
-                  <div className="category-time">
-                    <span className="category">디저트 </span> <span>| {time} </span>
-                  </div>
-                  <div className="like-box">
-                    <span className="like" onClick={(e) => { likeCount(i) }}>🧡 {like[i]}</span> &nbsp;
-                    <FontAwesomeIcon icon={ faComment } /* color="gray" */ /> 0
-                    </div>
-                  </div>
-                <hr />
-              </article>
-            )
-          })
-          
-      }
+      {title.map(function (a, i) {
+        return (
+          <article
+            onClick={() => {
+              setModal(!modal);
+              setObject(i);
+            }}
+          >
+            <h3 className="title"> {title[i]} </h3>
+            <div className="subtitle">
+              <div className="category-time">
+                <span className="category">디저트 </span> <span>| {time} </span>
+              </div>
+              <div className="like-box">
+                <span
+                  className="like"
+                  onClick={(e) => {
+                    likeCount(i);
+                  }}
+                >
+                  🧡 {like[i]}
+                </span>{" "}
+                &nbsp;
+                <FontAwesomeIcon icon={faComment} /* color="gray" */ /> 0
+              </div>
+            </div>
+            <hr />
+          </article>
+        );
+      })}
 
-      <div className="title__modal">
-        <span className="category">디저트 </span>
-        <h3 className="title"> {title[0]} </h3>
-        <div className="category-time">
-                     <span> 푸딩비 | {time} </span>
-        </div>
-        
-        <hr />
+      {modal == true ? (
+        <Modal
+          time={time}
+          title={title}
+          bgColor={bgColor}
+          modalLetter={modalLetter}
+          object={object}
+        />
+      ) : null}
+
+      <div className="nav__modal">
+        <ul className="nav-title">
+          <li className="bold">카테고리 전체</li>
+          <li className="bold">디저트</li>
+          <li className="bold">레시피</li>
+          <li>맛있는 음료</li>
+          <li>베이킹</li>
+        </ul>
       </div>
-        
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div
+      className="letter__modal"
+      style={{ backgroundColor: props.bgColor[props.object] }}>
+      <span className="category">디저트 </span>
+      <h3 className="title letter__modal--title">
+        {props.title[props.object]}
+      </h3>
+      <div className="category-time">
+        <span> 푸딩비 | {props.time} </span>
+      </div>
+      <hr />
+      <div className="modal-content">
+        {props.modalLetter[Object.keys(props.modalLetter)[props.object]]}
+      </div>
     </div>
   );
 }
